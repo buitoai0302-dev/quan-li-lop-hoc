b107dad05aaab702c505cd2381b8d6a12552b131
buitoai0302-dev <buitoai0302@gmail.com>
2026-05-05 07:50:30 +0700

    Remove temporary test scripts

--- backend/test_memory.js
+++
@@ -1,3 +0,0 @@
-require('dotenv').config({ path: '.env' });
-const { createClass } = require('./src/controllers/class.controller.ts');
-// ts-node is needed to run TS file, but I can just use a tiny TS runner.
